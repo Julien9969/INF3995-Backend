a077cfbb643bedd798541ec538eb47d30312cd4c
Trizotto <julien.roux404@gmail.com>
2024-01-25 12:51:51 -0500

    fix database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@db/table"
+SQLALCHEMY_DATABASE_URL = "postgresql://eq102:root@db/inf3995"
 Base = declarative_base()
 
 engine = create_engine(
