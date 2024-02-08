8207c167d6a2e107321167b5fbb76888e8f7d53e
MajeedAB <majeedbaki@outlook.com>
2024-02-08 14:26:22 -0500

    ros pkg generated files for backend

---
+++ app/ros_nodes/src/backend_server/setup.py
@@ -0,0 +1,60 @@
+from setuptools import find_packages, setup
+
+import os
+from glob import glob
+
+package_name = 'backend_server'
+
+df = [
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # ('share/' + package_name + '/api', ['backend_server/api/.']),    
+        # ('share/' + package_name + '/db', ['backend_server/db/.']),    
+        # ('share/' + package_name + '/db/models', ['backend_server/db/models/.']),    
+    ]
+
+def package_files(data_files, directory_list):
+
+    paths_dict = {}
+
+    for directory in directory_list:
+
+        for (path, directories, filenames) in os.walk(directory):
+
+            for filename in filenames:
+                print("============ ", filename)
+
+                file_path = os.path.join(path, filename)
+                install_path = os.path.join('share', package_name, path)
+
+                if install_path in paths_dict.keys():
+                    paths_dict[install_path].append(file_path)
+
+                else:
+                    paths_dict[install_path] = [file_path]
+
+    for key in paths_dict.keys():
+        data_files.append((key, paths_dict[key]))
+
+    return data_files
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    
+    data_files=package_files(df, ['api/', 'db/']),
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+                'serv = backend_server.main:main',
+        ],
+    },
+)
