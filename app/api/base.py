17849933a3ce9518f30abfbe7ff85e5e11c900a8
victorgilbertp <zevictos@gmail.com>
2024-02-08 12:21:26 -0500

    setup d'une classe de base de websocket de fastapi

--- app/api/base.py
+++ app/api/base.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from app.api.examples import routes_crud
 from app.api.ping import routes_ping
+from app.api.websocket import route_mission
 
 api_router = APIRouter()
 api_router.include_router(routes_crud.router, prefix="/api/something", tags=["exemple-routes"])
 api_router.include_router(routes_ping.router, prefix="/api/ping", tags=["ping-routes"])
+api_router.include_router(route_mission.router)
