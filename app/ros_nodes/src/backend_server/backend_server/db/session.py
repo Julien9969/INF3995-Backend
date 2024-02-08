987d8e6c7a24b0e37e6164a267ad32c87f5359f2
MajeedAB <majeedbaki@outlook.com>
2024-02-08 14:26:07 -0500

    created ros pkg with backend src code

---
+++ app/ros_nodes/src/backend_server/backend_server/db/session.py
@@ -0,0 +1,29 @@
+import time
+from typing import Generator
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+while True:
+	try:
+		SQLALCHEMY_DATABASE_URL = "postgresql://eq102:root@localhost/inf3995"
+		Base = declarative_base()
+
+		engine = create_engine(
+			SQLALCHEMY_DATABASE_URL
+		)
+		SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+		time.sleep(1)
+		break
+	except Exception as e:
+		print("Waiting for db to be ready...")
+		continue
+     
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
---
+++ app/ros_nodes/src/backend_server/backend_server/main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+
+from backend_server.db.models.exemples_models import Base
+from backend_server.db.utils import check_db_connected, check_db_disconnected
+from backend_server.db.session import engine
+from backend_server.api.base import api_router
+# Don't work for the moment, on_event work of you want
+@asynccontextmanager
+async def app_lifespan(app: FastAPI):
+    # Start up event
+    print("Starting up")
+    await check_db_connected()
+    print(app.title)
+    
+    yield
+    
+    # Shutdown event
+    await check_db_disconnected()
+
+def include_router(app):
+    app.include_router(api_router)
+
+# def configure_static(app):
+#     app.mount("/static", StaticFiles(directory="static"), name="static")
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+def start_application() -> FastAPI:
+    app = FastAPI(lifespan=app_lifespan, debug=True, title="API", version="0.1")
+    include_router(app)
+    # configure_static(app)
+    create_tables()
+    return app
+
+# ENLEVE TEMPORAIREMENT POUR ETRE LANCE PAR ROS A LA PLACE
+app = start_application()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# @app.on_event("startup")
+# async def app_startup():
+#     await check_db_connected()
+# 
+# 
+# @app.on_event("shutdown")
+# async def app_shutdown():
+#     await check_db_disconnected()
