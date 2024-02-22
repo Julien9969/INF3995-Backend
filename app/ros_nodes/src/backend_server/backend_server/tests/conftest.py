f913ae7d12ea84ca8419aa9a309ad0a7ef586e54
Trizotto <julien.roux404@gmail.com>
2024-02-22 11:45:36 -0500

    chore: fix pipline db

--- app/ros_nodes/src/backend_server/backend_server/tests/conftest.py
+++ app/ros_nodes/src/backend_server/backend_server/tests/conftest.py
@@ -26,10 +26,8 @@ patch.dict("sys.modules", geometry_msgs=geometry_msgs_mock).start()
 import backend_server.tests.mock.interfaces_mock as interfaces_mock
 patch.dict("sys.modules", interfaces=interfaces_mock).start()
 
-<<<<<<< HEAD
 from backend_server.api.base import api_router
 
-=======
 # ROS2 Mock (make ros2 installation not required for testing)
 import backend_server.tests.mock.rclpy_mock as rclpy_mock
 patch.dict("sys.modules", rclpy=rclpy_mock).start()
@@ -42,7 +40,6 @@ patch.dict("sys.modules", interfaces=interfaces_mock).start()
 
 from backend_server.api.base import api_router
 
->>>>>>> dev
 def start_application():
     app = FastAPI()
     app.include_router(api_router)
@@ -58,13 +55,9 @@ while True:
         environment = os.getenv("SQLALCHEMY_DATABASE_HOST", "test_db")
         
         if environment == "test_db":
-            SQLALCHEMY_DATABASE_URL = "postgresql://test_eq102:test_root@test_db:5430/test_inf3995"
+            SQLALCHEMY_DATABASE_URL = "postgresql://eq102:root@test_db:5430/inf3995"
         else:
-<<<<<<< HEAD
             SQLALCHEMY_DATABASE_URL = f"postgresql://eq102:root@{environment}:5430/inf3995"        
-=======
-            SQLALCHEMY_DATABASE_URL = "postgresql://eq102:root@host.docker.internal:5430/inf3995"        
->>>>>>> dev
 
         print(f"Using database: {SQLALCHEMY_DATABASE_URL}")
         engine = create_engine(
