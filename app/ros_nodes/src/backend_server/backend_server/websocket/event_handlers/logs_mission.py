a98f9c6b5c042fb8b9a3e04d2501839a7ac43feb
Abdel <abdel-madjid.santanna@polymtl.ca>
2024-03-14 02:42:45 -0400

    [feat] updated mission loge node

--- app/ros_nodes/src/backend_server/backend_server/websocket/event_handlers/logs_mission.py
+++ app/ros_nodes/src/backend_server/backend_server/websocket/event_handlers/logs_mission.py
@@ -1,9 +1,13 @@
+import asyncio
+from backend_server.api.mission.mission_base import MissionState, MissionData
 from ..logs import send_log
+import logging
 import rclpy
 from rclpy.node import Node
 
 from rcl_interfaces.msg import Log
 
+logging.basicConfig(level=logging.DEBUG)
 class LogSubscriber(Node):
 
     def __init__(self):
@@ -28,10 +32,13 @@ class LogSubscriber(Node):
         severity = severity_levels.get(log_msg.level, 'UNKNOWN')
 
         # Extract robot ID from logger name
-        robot_id = log_msg.name.split("robot")[1][0]
+        if log_msg.name.split("robot") is not None and 1 < len(log_msg.name.split("robot")):
+            robot_id = log_msg.name.split("robot")[1][0] 
+        else:
+            robot_id = 2
 
         # Extract log message
-        message = log_msg.msg
+        message = log_msg.data
 
         #Extract log name
         name = log_msg.name
@@ -39,6 +46,7 @@ class LogSubscriber(Node):
         return severity, robot_id, name, message
 
     def listener_callback(self, msg):
+        logging.debug("This is a debug message.")
         severity, robot_id,name, message = self.parse_log_message(msg)
         log_message = f"{severity}: {name}: {message}"
         # self.get_logger().info(robot_id)
@@ -48,18 +56,21 @@ class LogSubscriber(Node):
 
         
         
-async def record_logs():
+async def start_record_logs():
     rclpy.init()
-    logSubscriber = LogSubscriber()
     current_index:int = 0
-    while(rclpy.ok):
+    while(MissionData().get_mission_state() == MissionState.ONGOING):
         try:
+            await asyncio.sleep(2)
+            logSubscriber = LogSubscriber()
             rclpy.spin_once(logSubscriber, timeout_sec=2)
+            # logging.debug("This is a debug message..............................")
             #TODO S'assurer qu'il y a vraiment une valeur de log enregistrée avant de send_log
             if current_index < len(logSubscriber.messageList) and logSubscriber.messageList[current_index] is not None:
                 await send_log(logSubscriber.messageList[current_index][0],
                            logSubscriber.messageList[current_index][1])
                 current_index += 1
+            logSubscriber.destroy_node()
         except KeyboardInterrupt:
             pass
 
