833ece7992df0a28243c1566d5559aa11bc706f2
Abdel <abdel-madjid.santanna@polymtl.ca>
2024-03-15 12:23:28 -0400

    [chore] cleaned code to be easier for tests

--- app/ros_nodes/src/backend_server/backend_server/websocket/event_handlers/logs_mission.py
+++ app/ros_nodes/src/backend_server/backend_server/websocket/event_handlers/logs_mission.py
@@ -1,6 +1,9 @@
 import asyncio
+from collections import namedtuple
+import re
+from typing import List
 from backend_server.api.mission.mission_base import MissionState, MissionData
-from ..logs import send_log
+from ..logs import LogType, send_log
 import logging
 import rclpy
 from rclpy.node import Node
@@ -8,9 +11,10 @@ from rclpy.node import Node
 from rcl_interfaces.msg import Log
 
 logging.basicConfig(level=logging.DEBUG)
+RosLog = namedtuple('RosLog', ['source_id', 'message', 'logType'])
 class LogSubscriber(Node):
 
-    def __init__(self, messageList):
+    def __init__(self):
         super().__init__('logs_subscriber')
         self.subscription = self.create_subscription(
             Log,
@@ -18,11 +22,21 @@ class LogSubscriber(Node):
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
-        self.messageList = messageList
+        self.lastRosLog : RosLog = None
         self.isFinished = False
+        self.isNewLog = False
 
-    def parse_log_message(self, log_msg):
-        # Extract severity level
+    def get_robot_id(self,log_msg):
+
+        pattern = r"robot(\d+)"
+        match = re.search(pattern, log_msg.name)
+        if match:
+            robot_id = match.group(1)
+        else:
+            robot_id = 0
+        return robot_id
+
+    def get_severity(self,log_msg):
         severity_levels = {
             10: 'DEBUG',
             20: 'INFO',
@@ -31,47 +45,41 @@ class LogSubscriber(Node):
             50: 'FATAL'
         }
         severity = severity_levels.get(log_msg.level, 'UNKNOWN')
-
-        # Extract robot ID from logger name
-        if log_msg.name.split("robot") is not None and 1 < len(log_msg.name.split("robot")):
-            robot_id = log_msg.name.split("robot")[1][0] 
+        return severity
+    
+    def get_event_type(self,log_msg):
+        if "command" in log_msg.msg:
+            event_type = LogType.COMMAND
         else:
-            robot_id = 2
+            event_type = LogType.LOG
+        return event_type
+        
 
-        # Extract log message
-        message = log_msg.msg
-        if message == 'CODE 36: STOP RECORDING': 
+    def listener_callback(self, raw_log):
+        logging.debug("listener called")
+        
+        # Check if the message indicates the end of the process
+        if raw_log.msg == 'Incoming request, command: stop, current state: State.ON': 
             self.isFinished = True
-
-        #Extract log name
-        name = log_msg.name
-
-        return severity, robot_id, name, message
-
-    def listener_callback(self, msg):
-        logging.debug("This is a debug message.")
-        severity, robot_id,name, message = self.parse_log_message(msg)
-        log_message = f"{severity}: {name}: {message}"
-        # self.get_logger().info(robot_id)
-        self.messageList.append([log_message, robot_id])
-        # self.get_logger().info(log_message)
+        
+        source_id : int = self.get_robot_id(raw_log)
+        logType = self.get_event_type(raw_log)
+        formatted_message = f"{self.get_severity(raw_log)}: {raw_log.name}: {raw_log.msg}"
+        
+        self.lastRosLog = RosLog(source_id, formatted_message, logType)
+        self.isNewLog = True
         
 
         
 async def start_record_logs():
-    # rclpy.init()
-    current_index:int = 0
-    messageList = []
-    logSubscriber = LogSubscriber(messageList)
+    logSubscriber = LogSubscriber()
     while(rclpy.ok()):
         try:
-            # logSubscriber = LogSubscriber(messageList)
             rclpy.spin_once(logSubscriber, timeout_sec=2)
-            if current_index < len(messageList) and messageList[current_index] is not None:
-                await send_log(messageList[current_index][0],
-                           messageList[current_index][1])
-                current_index += 1
-            # logSubscriber.destroy_node()
+            if logSubscriber.isNewLog and logSubscriber.lastRosLog is not None:
+                log = logSubscriber.lastRosLog
+                await send_log(log.message, log.source_id, log.logType)
+                logSubscriber.isNewLog = False
             await asyncio.sleep(2)
             if (logSubscriber.isFinished): break
         except KeyboardInterrupt:
