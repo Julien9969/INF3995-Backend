cc0edcebf64b93314b0f196d248a9d2864d55e80
Majeed ABaki <majeed.abdul-baki@polymtl.ca>
2024-03-22 00:43:28 +0000

    Feature/map

--- app/ros_nodes/src/backend_server/backend_server/websocket/event_handlers/mission.py
+++ app/ros_nodes/src/backend_server/backend_server/websocket/event_handlers/mission.py
@@ -1,3 +1,5 @@
+import asyncio
+import logging
 import threading
 from .logs_mission import LogManager
 from backend_server.websocket.base import sio
@@ -5,7 +7,7 @@ from backend_server.websocket.events import Events
 from backend_server.api.mission.mission_base import start_mission, stop_mission
 from backend_server.websocket.logs import send_log, LogType, Log
 from backend_server.websocket.status import StatusUpdate
-
+from backend_server.websocket.event_handlers.map import MapManager
 
 @sio.on(Events.MISSION_STATUS.value)
 async def get_mission_status(sid):
@@ -16,16 +18,26 @@ async def get_mission_status(sid):
     assert status is not None
     await sio.emit(Events.MISSION_STATUS.value, status.to_json(), to=sid)
 
-
 @sio.on(Events.MISSION_START.value)
 async def set_mission_start(sid, _=None):
     """
     Confirm to clients that the mission has been started
     """
+    logging.debug("Starting mission")
     result = start_mission()
     await sio.emit(Events.MISSION_START.value)
     await send_log(f"Robots response to start: {result}")
     await LogManager.start_record_logs()
+
+@sio.on(Events.MISSION_MAP.value)
+async def start_record_map(sid, _=None):
+    """
+    Start sending map to client
+    """
+    logging.debug("Map")
+    await sio.emit(Events.MISSION_MAP.value)
+    await send_log(f"Start receive map")
+    await MapManager.start_map_listener()
 
 
 
@@ -38,8 +50,8 @@ async def set_mission_end(sid, _=None):
     await send_log(f"Robots response to stop: {result}")
     await sio.emit(Events.MISSION_END.value)
     LogManager.stop_record_logs()
-
-
+    MapManager.stop_map_listener()
+    
 # Debug: notify the frontend that it should
 # reset the mission state because state was lost during hot reload
 sio.emit(Events.MISSION_RESTART.value)
