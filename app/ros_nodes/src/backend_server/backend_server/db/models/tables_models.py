2e56fb13cfdfe6ceb57a095c1ad81855d0e76b34
Abdel <abdel-madjid.santanna@polymtl.ca>
2024-03-28 14:34:13 -0400

    [chore] removed unused example files and tables

--- app/ros_nodes/src/backend_server/backend_server/db/models/tables_models.py
+++ app/ros_nodes/src/backend_server/backend_server/db/models/tables_models.py
@@ -1,23 +1,8 @@
-from sqlalchemy import Enum, Column, Integer, String, ForeignKey
+from sqlalchemy import Enum, Column, Integer, String, ForeignKey, , DateTime
 from sqlalchemy.orm import relationship
 from backend_server.db.session import Base, SessionLocal
 from backend_server.schemas.something_type import SomethingType
 
-
-class Something(Base):
-    __tablename__ = "something"
-
-    id = Column(Integer, primary_key=True)
-    # other_id = Column(Integer, ForeignKey("othertable.id"), nullable=False)
-    truc = Column(Enum(SomethingType), nullable=False)
-
-
-class stringTable(Base):
-    # should be lowercase because with capital letters the name of table will have double quotes (select * from "stringTable"; will be the query)
-    __tablename__ = "stringtable" 
-
-    id = Column(Integer, primary_key=True)
-    txt = Column(String, nullable=False)
 
 # Define the Log model
 class Log(Base):
