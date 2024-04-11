06766287a1ca3003c884cc28c6feb4411e05cb28
ehourdebaigt <51237571+ehourdebaigt@users.noreply.github.com>
2024-04-11 17:16:56 -0400

    - Add detection robots

--- app/ros_nodes/src/backend_server/backend_server/api/identify/identify_client.py
+++ app/ros_nodes/src/backend_server/backend_server/api/identify/identify_client.py
@@ -1,7 +1,7 @@
-import sys
 from interfaces.srv import Identify
 import rclpy
 from rclpy.node import Node
+
 
 class IdentifyClientAsync(Node):
     """
@@ -9,13 +9,14 @@ class IdentifyClientAsync(Node):
     """
     def __init__(self, robot_id: int = 1):
         super().__init__('identify_client_async')
+        self.future = None
         ros_route = f"robot{robot_id}/identify"
         self.cli = self.create_client(Identify, ros_route)
 
         if self.cli.wait_for_service(timeout_sec=5.0):
             self.req = Identify.Request()
         else:    
-            self.get_logger().info(f'service not available (robot id {robot_id}), waiting again...')
+            self.get_logger().info(f'service not available (robot id {robot_id})...')
 
     async def send_request(self, a):
         self.req.a = a
